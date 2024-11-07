4dc52e94a61a3f37b388a41c43feac3c6c2357c4
mikle <mikle.kontinent@gmail.com>
2024-11-07 10:35:26 +0700

    Initial commit

---
+++ PersonalQuiz.animal/PersonalQuiz.animal/ViewControllers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PersonalQuiz.animal
+//
+//  Created by Таня Кожевникова on 07.11.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
